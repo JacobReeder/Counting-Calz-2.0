86735d052642850af2ebf257feb3aeadc0e03199
JacobReeder <jacob.reeder1@gmail.com>
2022-06-29 21:27:35 -0700

    Updated resolvers and TypeDefs; tested routes in GraphQl + routes return seeded data.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,18 +1,28 @@
-const { User } = require('../models');
+const { User, Post } = require('../models');
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find()
-        .select('-__v -password');
+        .select('-__v -password')
+        .populate('posts');
      
     },
     user: async (parent, { username }) => {
       return User.findOne({ username })
-        .select('-__v -password');
+        .select('-__v -password')
+        .populate('post');
         
     },
-   
+
+    posts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Post.find(params).sort({ /*createdAt: -1*/ });
+    },
+    
+    post: async (parent, { _id }) => {
+      return Post.findOne({ _id });
+    }
   }
 };
   
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,15 +7,27 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
+    posts: [Post]
    
   }
 
+  type Post {
+    _id: ID
+    description: String
+    user_id: String
+  }
+  
+ 
   type Query {
     users: [User]
     user(username: String!): User
-   
+    posts(username: String): [Post]
+    post(_id: ID!): Post
   }
 `;
 
+
+//calories: Int   **Put back into Post once completed
+//    date_time: String **Put back into post once completed
 // export the typeDefs
 module.exports = typeDefs;
