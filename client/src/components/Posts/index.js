60a5787b9c951b54369b0424b2cb614f63222fd2
Deck Stream <tneswick2@verizon.net>
2022-07-05 18:40:29 -0700

    meals page look completed

--- client/src/components/Posts/index.js
+++ client/src/components/Posts/index.js
@@ -6,7 +6,7 @@ const Posts = () => {
       <ul className="posts-list">
         <li className="list-el">
           <div className="post-date-desc-wrap">
-            <h6 className="post-date">12/25/1999</h6>
+            <h6 className="post-date">12/25/1999 at 05:15PM</h6>
             <h5 className="post-description">Christmas Dinner</h5>
           </div>
           <div className="calories">950 cal</div>
