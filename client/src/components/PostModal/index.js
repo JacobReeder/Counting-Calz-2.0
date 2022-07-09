d8b15ab2866c627dc6705f180a540648ee2a6627
Deck Stream <tneswick2@verizon.net>
2022-07-09 13:54:25 -0700

    push for team to work on postmodal pushing to posts and edit goal editing the user goal

--- client/src/components/PostModal/index.js
+++ client/src/components/PostModal/index.js
@@ -1,27 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+
+import { ADD_POST } from '../../utils/mutations'
 
 const PostModal = ({ onClose }) => {
+  const [ formState, setFormState ] = useState({ calories: '', description: '', dateTime: '' });
+
+  const [addPost, { error }] = useMutation(ADD_POST);
+
+  const handleFormChange = event => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value
+    });
+  }
+
+  const handleFormSubmit = async event => {
+    event.preventDefault()
+
+    try {
+      await addPost({
+        variables: { ...formState }
+      });
+
+      setFormState({ calories: '', description: '', dateTime: '' })
+
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
   return (
     <div className="backdrop">
       <div className="modalContainer">
         <h3 className="modalTitle">New Meal</h3>
-        <form id="post-form">
+        <form id="post-form" onSubmit={handleFormSubmit}>
           <div>
-            <label htmlFor="count">Calories</label>
+            <label htmlFor="calories">Calories</label>
             <br></br>
-            <input className="modal-input" type="text" name="count"/>
+            <input 
+            className="modal-input" 
+            type="text" 
+            name="calories"
+            onChange={handleFormChange}
+            />
           </div>
           <div>
-            <label htmlFor="desc">Meal Description</label>
+            <label htmlFor="description">Meal Description</label>
             <br></br>
-            <input className="modal-input" type="text" name="desc" />
+            <input 
+            className="modal-input" 
+            type="text" 
+            name="description" 
+            onChange={handleFormChange}
+            />
           </div>
           <div>
-            <label htmlFor="Time" className="fw-bold">When did you have this meal?</label>
-            <input type="datetime-local"/>
+            <label htmlFor="dateTime" className="fw-bold">When did you have this meal?</label>
+            <input 
+            type="datetime-local" 
+            name="dateTime"
+            onChange={handleFormChange}
+            />
           </div>
-          {/* error message */}
+          {error && <div>Error in posting, please try again</div>}
           <button className="edit-goal-btn btn-padding" type="submit">Save</button>
           <button className="edit-goal-btn btn-padding" type="click" onClick={onClose}>Cancel</button>
         </form>
